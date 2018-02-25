e24b344b72e4eff278bff981b92dc7873b10e74b
Thomas Chabaud <tchabaud@chabs.name>
2018-02-25 10:05:30 +0100

    Update grav image to add git sync plugin.

--- grav/entrypoint.sh
+++ grav/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
 umask 0002
+
+cd ${GRAV_HOME}
+./bin/gpm install -y git-sync
+chown -R nginx.www-data ${GRAV_HOME}
+echo "Permissions updated."
 /usr/sbin/php-fpm7 --fpm-config /etc/php7/php-fpm.d/www.conf
 /usr/sbin/nginx -g "daemon off;"
